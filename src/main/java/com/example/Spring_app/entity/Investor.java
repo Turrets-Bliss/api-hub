02eb8d88ea7a73a0a39707d39948c1dea47c6cca
aayushsrivastava0 <aayush.srivastava@nashtechglobal.com>
2025-01-31 15:09:28 +0530

    Bug fix removed redundant entity  due to which API's were failing on new build

--- src/main/java/com/example/Spring_app/entity/Investor.java
+++ src/main/java/com/example/Spring_app/entity/Investor.java
@@ -8,9 +8,6 @@ import java.math.BigDecimal;
 public class Investor {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // This might not exist in the schema but can be ignored if not mapped
-
     @Column(name = "user_id", nullable = false)
     private Long userId;
 
@@ -30,15 +27,6 @@ public class Investor {
     private BigDecimal roi;
 
     // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Long getUserId() {
         return userId;
     }
